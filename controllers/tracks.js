aaeba8af9b167a233ea112fe27b138e8ca2f5d46
tflinch <tflinch16@gmail.com>
2024-07-14 19:35:05 -0400

    feat: get route- read tracks from database

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/tracks', (req, res) => {
-console.log(req.body.name);
-res.end()
-})
+// Data Import
+const { Track } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const tracksFound = await Track.find({});
+        if (tracksFound) {
+            console.log('found track', tracksFound);
+        }
+        console.log(req.body.name);
+        res.status(200).json(tracksFound);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+router.post('/', async (req, res) => {
+    console.log(req.body)
+    res.end()
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const methodOverride = require('method-override')
 const PORT = process.env.PORT || 3000;
 
-
+app.use(cors())
 app.use(methodOverride("_method"));
 app.use(express.json());
 
+app.get('/', (req, res)=>{
+    res.send({home: 'homepage'})
+})
 
-app.use('/tracks', require('/controllers/tracks'));
+app.use('/tracks', require("./controllers/tracks"));
 
 const server = app.listen(PORT, () =>{
     console.log("🚀 Server Listening on PORT:", PORT)
